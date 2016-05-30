5238b3ee703bba8b732effa4e59e7842fb1bc97f
purinxxx <kengo@MacBook-Pro.local>
2016-05-30 17:22:01 +0900

    譜面ファイル読み込み

--- Assets/Script/humen.cs
+++ Assets/Script/humen.cs
@@ -11,6 +11,9 @@ public class humen : MonoBehaviour {
 	GameObject w;
 	GameObject k;
 
+	string txt = "";
+	string[] arrtxt;
+
 	// Use this for initialization
 	void Start () {
 		RaneController = GameObject.Find("RaneController");
@@ -18,7 +21,6 @@ public class humen : MonoBehaviour {
 		wood = (GameObject)Resources.Load ("Prefabs/wood");
 		kugi = (GameObject)Resources.Load ("Prefabs/kugi");
 		loadhumen ();
-
 	}
 	
 	// Update is called once per frame
@@ -27,17 +29,36 @@ public class humen : MonoBehaviour {
 	}
 
 	void loadhumen() {
-		float bpm = 150;
-		float[] humen = {0,1,1,0,1,1,2,2,1,2,2,0,1,1,2,2,1,2,2,0};
+		//float bpm = 150;
+		//float[] humen = {0,1,1,0,1,1,2,2,1,2,2,0,1,1,2,2,1,2,2,0};
+		FileInfo fi = new FileInfo(Application.dataPath + "/" + "test.txt");
+		try {
+			// 一行毎読み込み
+			using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8)){
+				txt = sr.ReadToEnd();
+				arrtxt = txt.Split(',');
+			}
+		} catch (Exception e){
+			// 改行コード
+			//guitxt += SetDefaultText();
+		}
+		//print (arrtxt[0]);
+		float bpm = float.Parse(arrtxt[0]);
+		//print (arrtxt[1]);
+		string[] humen = new string	[arrtxt[1].Length];
+		for (int i = 0; i < arrtxt[1].Length; i++) humen[i] = arrtxt[1].Substring(i, 1);
+		//print (bpm);
+		//print (humen[1]);
 		createhumen (bpm, humen);
 	}
 
-	void createhumen(float bpm, float[] arr) {
+	void createhumen(float bpm, string[] arr) {
 		float j = 0;
 		float kugikankaku = 2 * 60 / bpm;
 		float x = 4*2*60/bpm;
-		foreach (float i in arr)
+		foreach (string i in arr)
 		{
+			int h = int.Parse (i);
 			if (j % 4 == 0) {
 				// wood作成
 				w = (GameObject)Instantiate (wood, new Vector3(-(x/2)-(x*j/4), 0.5f, 0), Quaternion.identity);
@@ -45,11 +66,11 @@ public class humen : MonoBehaviour {
 				w.transform.localScale = new Vector3(x, 1, 1);
 			}
 			//print (i);
-			if (i == 1) {
+			if (h == 1) {
 				// kugi作成
 				k = (GameObject)Instantiate (kugi, new Vector3((-1-j)*kugikankaku, 1, 0), Quaternion.identity);
 				k.transform.parent = RaneController.transform;
-			}else if(i==2){
+			}else if(h == 2){
 				//ookii kugi
 				k = (GameObject)Instantiate (kugi, new Vector3((-1-j)*kugikankaku, 1.25f, 0), Quaternion.identity);
 				k.transform.parent = RaneController.transform;
